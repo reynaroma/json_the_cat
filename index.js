9d51e20aa9ba37479f8d2fa2eecf0a010943bd18
Reyna May <roma.reynamayA@gmail.com>
2023-07-06 17:12:45 -0600

    Separate the module and function of fetchBreedDescription and exports the function

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+// Get the breed name from command-line arguments
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
